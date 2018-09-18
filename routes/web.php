a98b2e4dac79a466b3b6b4798d16de7f7f0bbf13
Malcolm Arcand Laliberté <malcolm@shredsauce.com>
2018-09-17 21:28:46 -0400

    Simple index page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'PageController@index');
+
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+/*
+Route::get('/home', function () {
+   	return '<h1>Hello World</h1>';
 });
+*/
