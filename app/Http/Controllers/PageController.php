702631f344320a7470f1dec89d59eecac4e0702a
Malcolm Arcand Laliberté <malcolm@shredsauce.com>
2018-09-19 13:05:24 -0400

    Get flight information including departure and arrival airports, and airline information

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,27 +7,38 @@ use Illuminate\Support\Facades\DB;
 
 class PageController extends Controller {
     public function index () {
-        return view('pages.index');
-    }
-
-    public function services() {
-        // $flights = DB::table('flights')->where('airline', 'AC');
-        // return view('pages.services', ['flights' => $flights]);
-
-
         $flights = DB::table('flights')->get();
-        return view ('pages.services')->with('flights', $flights); 
 
-        // $flights = DB::table('flights')->get();
-        // return view('pages.services', ['flights' => $flights]);
+        // Fetch the full airport and airline data for each flight
+        foreach ($flights as $flight) {
+            $flight->airline = DB::table('airports')->where('code', '=', $flight->airline)->first();
+            $flight->departure_airport = DB::table('airports')->where('code', '=', $flight->departure_airport)->first();
+            $flight->arrival_airport = DB::table('airports')->where('code', '=', $flight->arrival_airport)->first();
+        }
+        
+        return view ('pages.index')->with('flights', $flights); 
     }
 
-    // public function services() {
-    // 	$data = array (
-    // 		'title' => 'Services',
-    // 		'services' => ['Blah', 'blah2', 'blah 3']
-    // 	);
-    // 	return view('pages.services')->with($data);
-    // }
+    // Get flight information from sqlite database
+    public function flights() {
+        $flights = DB::table('flights')->get();
+        $airlines = DB::table('airlines')->get();
+        $airports = DB::table('airports')->get();
 
+        $data = array('flights' => $flights);
+
+        $data = array(
+            'flights'  => $flights,
+            'airlines' => $airlines,
+            'airports' => $airports
+        );
+
+        return view ('pages.flights')->with('data', $data); 
+    }
+
+   // Get flight information from sqlite database
+    public function flights2() {
+        $flights2 = "this is flights 2";
+        return view ('pages.flights')->with('flights2', $flights2); 
+    }    
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,9 +1,52 @@
+<?php use App\Http\Controllers\FlightController;?>
+
 @extends('layouts.app')
 
 @section('content')
 	<div class="jumbotron text-center">
 		<h1>{{config('app.name', 'Trip Builder')}}</h1>
 		<p>Build a trip</p>
+
+		<a href="{{ url('/services/') }}" class="btn btn-xs btn-info pull-right">Sort</a>
+		<a href="{{ url('/services/') }}" class="btn btn-xs btn-info pull-right">Sort</a>
+
+{{-- 		@if(count($data['flights']) > 0)
+			<ul class="list-group">
+				@foreach($data['flights'] as $flight)
+					<li class="list-group-item">
+						<div class="column">
+						{{$flight->airline}}
+
+						</div>
+<div class="column">
+						{{$flight->number}}
+
+						</div>						
+					</li>
+				@endforeach
+			</ul>
+		@endif
+ --}}
+
+		@if(count($flights) > 0)
+			<ul class="list-group">
+				@foreach($flights as $flight)
+					<li class="list-group-item">
+						<div class="row">
+							${{$flight->price}}
+						</div>
+						<ol>
+							Departure: {{$flight->departure_airport->city}} ({{$flight->departure_airport->code}})
+							at {{$flight->departure_time}}
+						</ol>
+						<ol>
+							Arrival: {{$flight->departure_airport->city}} ({{$flight->departure_airport->code}})
+							at {{$flight->arrival_time}}
+						</ol>						
+					</li>
+				@endforeach
+			</ul>
+		@endif
 
 
 		{{-- Calendar --}}
